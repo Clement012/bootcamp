50636b853c4ad2b33799da38c54051b7836d8a0f
Clement012 <164832311+Clement012@users.noreply.github.com>
2024-04-22 13:36:40 +0800

    Delete datatype directory

--- datatype/src/Helloworld.java
+++
@@ -1,7 +0,0 @@
-public class Helloworld{
-    public static void main(String[] args){
-        //start point of the program hello
-        System.out.println("Hello");
-        System.out.println("ClementYAU");
-    }
-}
